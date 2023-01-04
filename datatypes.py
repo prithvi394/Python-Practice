b4b4a9bc1d6e678868a07dc3de2436ccce46c89f
prithvirajgangadharaiah <prithviraj394@yahoo.com>
2023-01-03 21:52:55 -0500

    Initial commit - Python practice

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+# string
+course_objective = "I'll master you"
+student_name = "Prithvi"
+final_message = course_objective + " - " + student_name
+
+# int
+int_num = 21
+
+# float
+float_num = 23.0
+
+# boolean
+is_boolean = True
+print("Hi python -> " + final_message)
+
+print(type(final_message))
+print(type(int_num))
+print(type(float_num))
+print(type(is_boolean))
---
+++ multiple_assingments_and_typecasting.py
@@ -0,0 +1,7 @@
+
+
+#multiple variable assignment with a single value
+hattu = ten = dias = 10
+
+print(str(dias) + "-" + str(ten) + "-" + str(hattu))
+
