73e2d3fd3ad78773325166918e199b04650b1dd0
prithvirajgangadharaiah <prithviraj394@yahoo.com>
2023-01-03 22:21:50 -0500

    Multiple assignment and typecasting

--- multiple_assingments_and_typecasting.py
+++ multiple_assingments_and_typecasting.py
@@ -1,7 +1,10 @@
 
 
-#multiple variable assignment with a single value
+# multiple variable assignment with a single value
 hattu = ten = dias = 10
-
 print(str(dias) + "-" + str(ten) + "-" + str(hattu))
 
+
+# multiple variable assignment in a single line
+user_name, age, is_above_18 = "Sam", 22, True
+print(user_name + "-" + str(age) + "-" + str(is_above_18))
